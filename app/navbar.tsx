f5f89f88c824c7a882cbca71dae484e32c640749
jp calvo <calvojp92@gmail.com>
2023-11-19 10:15:00 +0800

    add logo

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -10,12 +10,37 @@ import {
 } from '@/components/tooltip';
 import {Flex, Spacer, VisuallyHidden, styled} from '@/styled-system/jsx';
 import {Portal} from '@ark-ui/react';
+import {Lilita_One} from 'next/font/google';
+
+const heading = Lilita_One({
+  weight: '400',
+  subsets: ['latin'],
+  preload: true,
+});
 
 export function Navbar() {
   return (
     <styled.header bg="neutral.800">
       <Flex h={24} px={12} alignItems="center" maxW="breakpoint-xl" mx="auto">
+        <styled.h3
+          color="transparent"
+          fontFamily="var(--font-heading)"
+          fontSize="5xl"
+          fontWeight="black"
+          lineHeight="none"
+          textTransform="uppercase"
+          bgGradient="to-r"
+          gradientFrom="pink.500"
+          gradientVia="orange.500"
+          gradientTo="purple.500"
+          backgroundClip="text"
+          className={heading.className}
+        >
+          Pokemon
+        </styled.h3>
+
         <Spacer />
+
         <Tooltip openDelay={0} closeDelay={0}>
           <TooltipTrigger asChild>
             <Link href="https://github.com/calvo-jp/pokemon">
